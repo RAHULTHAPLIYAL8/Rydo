c446df654bc3dfcfcd3f3872557be8d9b9fc9bea
RAHULTHAPLIYAL8 <rahulthapliyal47@gmail.com>
2026-03-18 20:20:19 +0530

    change local mongdb server into dynamic using env file

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,7 +1,13 @@
 const mongoose=require("mongoose");
 function connectToDb()
 {
-    mongoose.connect("mongodb://127.0.0.1:27017/uber").
-    then(console.log("Mongod Db Connected Successfully")).catch((err)=>{console.log(err)})
+    // mongoose.connect("mongodb://127.0.0.1:27017/uber").
+        mongoose.connect(process.env.MONGO_URL)
+        .then(() => {
+            console.log("MongoDB Connected Successfully");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 }
 module.exports=connectToDb;
